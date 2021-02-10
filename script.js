8ee12eb109e7c9059c55aedc0aa08b1c64b04c5d
MatildaMared <matildamared@live.se>
2021-02-10 08:35:28 +0100

    add javacsript to make hamburger menu close on click

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let checkbox = document.querySelector("#navigation__toggle");
 let hamburger = document.querySelector("#navigation__label");
 
-console.log(checkbox);
-console.log(hamburger);
+checkbox.addEventListener( "click", function(){
+    if (this.checked) {
+        document.addEventListener("click", listener);
+    }
+});
+
+let listener = function(e) {
+    if (e.target != checkbox && e.target != hamburger) {
+        checkbox.checked = false;
+        document.removeEventListener("click", listener);
+    }
+};
